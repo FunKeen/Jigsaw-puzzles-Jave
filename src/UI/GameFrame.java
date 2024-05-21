2f0055c22eb33ac2aa3d8f8e23aa2dab6c8ff65c
FunKeen <1987680880@qq.com>
2024-05-21 16:53:19 +0800

    first

--- src/UI/GameFrame.java
+++ src/UI/GameFrame.java
@@ -8,26 +8,29 @@ import java.util.Random;
 
 public class GameFrame extends JFrame implements MouseListener {
     //记录鼠标点击、松开位置
-    int xp;
-    int yp;
-    int xr;
-    int yr;
+    int mp;
+    int np;
+    int mr;
+    int nr;
     //移动的步数
     int step;
+    //拼图块大小
+    int size;
     //创建初始拼图位置
     int[] tempArr;
     //创建4*4大小的拼图块
     int[][] arr = new int[4][4];
     //记录空白块的位置，默认3，3
-    int emptyX;
-    int emptyY;
+    int emptyM;
+    int emptyN;
 
     public void initData() {
         //初始化数值
+        size = new ImageIcon("src/image/miku1/1.jpg").getIconWidth();
         tempArr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
         step = 0;
-        emptyX = 3;
-        emptyY = 3;
+        emptyM = 3;
+        emptyN = 3;
     }
 
     //界面初始化函数（GameFrame的构造函数）
@@ -53,6 +56,7 @@ public class GameFrame extends JFrame implements MouseListener {
         this.setSize(1097, 857);
         this.setLocationRelativeTo(null);
         this.setLayout(null);
+        this.addMouseListener(this);
     }
 
     //初始化游戏菜单栏
@@ -103,18 +107,16 @@ public class GameFrame extends JFrame implements MouseListener {
         label.setBounds(23, 10, 256, 256);
         this.getContentPane().add(label);
 
-        label=new JLabel("步数:"+step);
-        label.setBounds(100, 300,100,30);
+        label = new JLabel("步数:" + step);
+        label.setBounds(100, 300, 100, 30);
         this.getContentPane().add(label);
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 ImageIcon icon1 = new ImageIcon("src/image/miku1/" + arr[i][j] + ".jpg");
-                int size = icon1.getIconHeight();
                 JLabel label1 = new JLabel(icon1);
                 label1.setBounds(300 + j * size, 10 + i * size, size, size);
                 label1.setBorder(new BevelBorder(BevelBorder.RAISED));
-                label1.addMouseListener(this);
                 this.getContentPane().add(label1);
             }
         }
@@ -148,13 +150,25 @@ public class GameFrame extends JFrame implements MouseListener {
         } else randomArray();
     }
 
+    //判断鼠标松开位置是否在灰色方块中
+    //用于优化操作
+    private boolean isArea(int m, int n) {
+        int m1 = 10 + emptyM * size;
+        int n1 = 300 + emptyN * size;
+
+        int m2 = m1 + size;
+        int n2 = n1 + size;
+
+        return m > m1 && m < m2 && n > n1 && n < n2;
+    }
+
     //移动拼图块
     private void moveXY(int X, int Y) {
         if (Y >= 0 && Y <= 3 && X >= 0 && X <= 3) {
-            arr[emptyX][emptyY] = arr[X][Y];
+            arr[emptyM][emptyN] = arr[X][Y];
             arr[X][Y] = 16;
-            emptyX = X;
-            emptyY = Y;
+            emptyM = X;
+            emptyN = Y;
             if (this.isWin()) {
                 System.out.println("You are win!!!");
             } else {
@@ -187,34 +201,38 @@ public class GameFrame extends JFrame implements MouseListener {
 
     @Override
     public void mousePressed(MouseEvent e) {
-        xp = e.getX();
-        yp = e.getY();
+        mp = e.getY();
+        np = e.getX();
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        xr = e.getX();
-        yr = e.getY();
-        int flag = Math.abs(xp - xr) - Math.abs(yp - yr);
-        int X = 0;
-        int Y = 0;
+        mr = e.getY();
+        nr = e.getX();
 
-        if (flag > 20) {
-            if (xp < xr) {
-                Y = emptyY - 1;
-            } else {
-                Y = emptyY + 1;
+        if (isArea(mr, nr)) {
+            int flag = Math.abs(mp - mr) - Math.abs(np - nr);
+            int X = 0;
+            int Y = 0;
+
+            if (flag < -20) {
+                if (np < nr) {
+                    Y = emptyN - 1;//右
+                } else {
+                    Y = emptyN + 1;//左
+                }
+                X = emptyM;
+                moveXY(X, Y);
+            } else if (flag > 20) {
+                if (mp < mr) {
+                    X = emptyM - 1;//下
+                } else {
+                    X = emptyM + 1;//上
+                }
+                Y = emptyN;
+                moveXY(X, Y);
             }
-            X = emptyX;
-        } else if (flag < -20) {
-            if (yp < yr) {
-                X = emptyX - 1;
-            } else {
-                X = emptyX + 1;
-            }
-            Y = emptyY;
         }
-        moveXY(X, Y);
     }
 
     @Override
